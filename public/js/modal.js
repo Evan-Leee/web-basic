f00f98a9ac67067042661098c694fe2e1f982f88
Evan <EvanLee_@outlook.com>
2015-08-13 22:24:57 +0800

    add public js

---
+++ public/js/modal.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+   $('button').on('click',function(){
+       if($('#class').val() === ''){
+           $('#class-modal').modal('show');
+           return ;
+       }
+       if($('#name').val() === ''){
+           $('#name-modal').modal('show');
+           return ;
+       }
+       if($('#stu_no').val() === ''){
+           $('#stu_modal').modal('show');
+           return ;
+       }
+
+       $('form').submit();
+
+   }) ;
+});
