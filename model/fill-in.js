98d0cf886ecb5fa53c259dfed622633931cc3e59
Evan <EvanLee_@outlook.com>
2015-08-19 10:19:07 +0800

    modify fill-in.js

---
+++ model/fill-in.js
@@ -0,0 +1,31 @@
+'use strict';
+var _ = require('lodash');
+var Topic = require('./topic');
+
+function Fillin(type,description,score,name,stdAnswer){
+    Topic.call(this,type,description,score,name,stdAnswer);
+}
+
+Fillin.prototype = Object.create(Topic.prototype);
+
+Fillin.prototype.constructor = Fillin;
+
+Fillin.prototype.getScore = function(form){
+
+    var score = 0;
+
+    if(Array.isArray(form[this.name])){
+        score += _.intersection(form[this.name],this.stdAnswer).length
+    }else{
+        if(form[this.name] === this.stdAnswer[0]){
+            score += this.score;
+        }
+    }
+
+
+    return score;
+
+};
+
+
+module.exports = Fillin;
