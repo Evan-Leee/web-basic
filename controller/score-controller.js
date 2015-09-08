24e8300f3471b6f275f5d22ee7f2991805870b14
Evan <EvanLee_@outlook.com>
2015-09-08 23:59:25 +0800

    change the score-controller to class

--- controller/score-controller.js
+++ controller/score-controller.js
@@ -1,12 +1,16 @@
 var Factory = require('../model/factory');
 var Score = require('../model/score');
 
-function setScore(req,res){
-    var factory = new Factory()
-    var questions = factory.setQuestions();
-    var score = Score.getTotal(req.body,questions);
+function ScoreController() {
 
-    res.render('index',{form:req.body,questions:questions,score:score})
 }
 
-module.exports = setScore;
+ScoreController.prototype.setScore = function (req, res) {
+    var factory = new Factory();
+    var questions = factory.setQuestions();
+    var score = Score.getTotal(req.body, questions);
+
+    res.render('index', {form: req.body, questions: questions, score: score})
+}
+
+module.exports = ScoreController;
